78962e07f5bfceefcc57940f3d2437bd98da4d1a
jeremy.zhou <gaolujie1989@126.com>
2014-09-16 09:42:59 +0800

    add cmd bat

---
+++ subtreeAdd.bat
@@ -0,0 +1,3 @@
+git remote add -f framework http://lujie.zhou@121.40.137.221:7990/scm/yin2/yincart2-framework.git
+git remote add -f basic http://lujie.zhou@121.40.137.221:7990/scm/yin2/yincart2-apps-basic.git
+git remote add -f advanced http://lujie.zhou@121.40.137.221:7990/scm/yin2/yincart2-apps-advanced.git
---
+++ subtreePush.bat
@@ -0,0 +1,3 @@
+git subtree push --prefix=framework framework master
+git subtree push --prefix=apps/basic basic master
+git subtree push --prefix=apps/advanced advanced master
