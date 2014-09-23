f3c5ebcf95bf2f86834c31e94515bd5ace21845a
jeremy.zhou <gaolujie1989@126.com>
2014-09-23 15:25:45 +0800

    to add auth

---
+++ framework/auth/controllers/backend/ItemController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Lujie.Zhou(lujie.zhou@jago-ag.cn) 
+ * @Date 9/22/2014
+ * @Time 11:05 AM
+ */
+
+namespace yincart\auth\controllers\backend;
+
+use yincart\base\web\Controller;
+
+class ItemController extends Controller
+{
+
+} 
---
+++ framework/auth/widgets/RoleTree.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Lujie.Zhou(lujie.zhou@jago-ag.cn) 
+ * @Date 9/22/2014
+ * @Time 3:37 PM
+ */
+
+namespace yincart\auth\widgets;
+
+use yincart\base\base\Widget;
+
+class RoleTree extends Widget
+{
+
+} 
